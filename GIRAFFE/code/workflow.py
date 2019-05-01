7884c1cad8d0a00bd25787daea3653894898e19b
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-05-01 23:48:38 +0200

    minimal pipeline

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -8,9 +8,24 @@ import nipype.pipeline as pe
 
 
 
+
+
+
+
+
+
+
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
-
+analysisflow.connect(FetchAnOrder, "output", RegionChoice, "input")
+analysisflow.connect(RegionChoice, "output", CreateOrderB, "input")
+analysisflow.connect(RegionChoice, "output", NoOrderPossible, "input")
+analysisflow.connect(RegionChoice, "output", UnservedRegion, "input")
+analysisflow.connect(CreateOrderB, "output", DatabaseError, "input")
+analysisflow.connect(NoOrderPossible, "output", OrderOK, "input")
+analysisflow.connect(CreateOrderB, "output", OrderOK, "input")
+analysisflow.connect(DatabaseError, "output", NoOrderPossible_1, "input")
+analysisflow.connect(UnservedRegion, "output", NoOrderPossible_1, "input")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
